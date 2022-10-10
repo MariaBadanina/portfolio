2d73be5ab66fcb753f228074cda2b9a853866c44
Maria Badanina <maria.badanina@secuoyas.com>
2022-10-10 22:18:22 +0200

    Add some noise

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import {
   MarchingCubes,
   OrbitControls,
   Sky,
+  Html,
 } from "@react-three/drei"
 import { Canvas, useFrame } from "@react-three/fiber"
-import { useRef } from "react"
+import { useRef, Suspense } from "react"
 import EnvironmentComponent from "./EnvironmentComponent"
+import {
+  EffectComposer,
+  DepthOfField,
+  Noise,
+  Vignette,
+} from "@react-three/postprocessing"
 
 const Sphere = ({ x, y, z, s }) => {
   const ref = useRef()
@@ -21,6 +28,13 @@ const Sphere = ({ x, y, z, s }) => {
 }
 
 const SpheresGroup = () => {
+  const ref = useRef()
+  useFrame(({ clock }) => {
+    ref.current.rotation.x = Math.sin(clock.elapsedTime / 2) * 0.2
+    ref.current.rotation.y = Math.sin(clock.elapsedTime / 2) * 0.2
+    ref.current.rotation.z = Math.sin(clock.elapsedTime / 2) * 0.2
+  })
+
   const data = new Array(10).fill().map((_, i) => ({
     x: Math.random() * 1 - 0.5,
     y: Math.random() * 1 - 0.5,
@@ -29,7 +43,7 @@ const SpheresGroup = () => {
   }))
 
   return (
-    <MarchingCubes resolution={64} enableColors>
+    <MarchingCubes resolution={64} enableColors ref={ref}>
       <meshStandardMaterial vertexColors roughness={0.2} />
       {data.map((props, i) => (
         <Sphere key={i} {...props} />
@@ -42,10 +56,23 @@ function App() {
   return (
     <>
       <Canvas camera={{ position: [0, 0, 5], fov: 25, near: 1, far: 40 }}>
-        <Sky />
-        <EnvironmentComponent />
-        <SpheresGroup />
-        <OrbitControls />
+        <Suspense fallback={<Html center>Loading.</Html>}>
+          <SpheresGroup />
+          <Sky />
+          <EnvironmentComponent />
+          <OrbitControls />
+        </Suspense>
+
+        <EffectComposer multisampling={0} disableNormalPass={true}>
+          <DepthOfField
+            focusDistance={0}
+            focalLength={0.02}
+            bokehScale={2}
+            height={480}
+          />
+          <Noise opacity={0.25} />
+          <Vignette eskil={false} offset={0.1} darkness={0.6} />
+        </EffectComposer>
       </Canvas>
     </>
   )
