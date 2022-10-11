fc9e0db94678922f1c3dca898cc560e661f1ba4d
Maria Badanina <maria.badanina@secuoyas.com>
2022-10-11 20:10:14 +0200

    WIP

--- src/Bg.js
+++ src/Bg.js
@@ -1,16 +1,5 @@
-import {
-  Bounds,
-  Html,
-  MarchingCube,
-  MarchingCubes,
-  OrbitControls,
-} from "@react-three/drei"
-import { Canvas, useFrame } from "@react-three/fiber"
-import { EffectComposer, Noise, Vignette } from "@react-three/postprocessing"
 import { Depth, LayerMaterial } from "lamina"
-import { Suspense, useRef } from "react"
 import * as THREE from "three"
-import EnvironmentComponent from "./EnvironmentComponent"
 
 const Bg = () => {
   return (
