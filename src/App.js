c6698bb15fd9d7b2f45f4d4b1231a617c58ff25e
Maria Badanina <maria.badanina@secuoyas.com>
2022-10-12 23:17:25 +0200

    Create some nice color layers

--- src/App.js
+++ src/App.js
@@ -9,21 +9,20 @@ import SphereGroup from "./components/SphereGroup"
 function App() {
   return (
     <>
-      <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 5], fov: 22 }}>
+      <Canvas camera={{ position: [0, 0, 3], fov: 22 }}>
         <Suspense fallback={<Html center>Loading.</Html>}>
           <SphereGroup />
           {/* Zoom to fit a 1/1/1 box to match the marching cubes */}
-          <Bounds fit clip observe margin={1}>
+          {/* <Bounds fit clip observe margin={1}>
             <mesh visible={false}>
               <boxGeometry />
             </mesh>
-          </Bounds>
+          </Bounds> */}
           <EnvironmentComponent />
-          <Bg />
-          <EffectComposer multisampling={0} disableNormalPass={true}>
-            <Noise opacity={0.25} />
-            <Vignette eskil={false} offset={0.1} darkness={0.6} />
-          </EffectComposer>
+          {/* <Bg /> */}
+          {/* <EffectComposer multisampling={0} disableNormalPass={true}>
+            <Noise opacity={0.5} />
+          </EffectComposer> */}
         </Suspense>
         <OrbitControls />
       </Canvas>
--- src/components/SphereGroup.js
+++ src/components/SphereGroup.js
@@ -1,6 +1,6 @@
 import { MarchingCube, MarchingCubes } from "@react-three/drei"
 import { useFrame } from "@react-three/fiber"
-import { Depth, LayerMaterial } from "lamina"
+import { Depth, DebugLayerMaterial, Fresnel } from "lamina"
 import { useRef } from "react"
 import Cursor from "./Cursor"
 
@@ -27,17 +27,70 @@ const SphereGroup = () => {
 
   return (
     <MarchingCubes resolution={64} enableColors>
-      <LayerMaterial lighting="standard" color="yellow" toneMapped={true}>
-        <Depth
-          colorA="#2A8AFF"
-          colorB="#ff4eb8"
+      <DebugLayerMaterial lighting="standard" toneMapped={true}>
+        {/* <Depth
+          colorA="#4214ae"
+          colorB="#ff5fc5"
           alpha={1}
-          mode="multiply"
+          mode="normal"
           near={0.0}
           far={0.9}
           origin={[0, 0, 0]}
+        /> */}
+        {/* <Depth
+          colorA="#b1344f"
+          colorB="#e44019"
+          alpha={1}
+          mode="normal"
+          near={0.0}
+          far={0.9}
+          origin={[0, 0, 0]}
+        /> */}
+
+        {/* <Depth
+          near={0}
+          far={0.5}
+          origin={[-0.5, 0.3, 0]}
+          colorA={"#fefb00"}
+          colorB={"#fe0000"}
+        /> */}
+
+        {/* <Depth
+          colorA="#ffcd54"
+          colorB="#fd8b88"
+          alpha={1}
+          mode="normal"
+          near={0}
+          far={0.5}
+          origin={[-0.5, 0.3, 0]}
+        /> */}
+        {/* 
+        <Depth
+          colorA="#b1344f"
+          colorB="#e44019"
+          alpha={1}
+          mode="normal"
+          near={0.5}
+          far={1}
+          origin={[0.3, -0.3, 0]}
+        /> */}
+        <Depth
+          colorA="#b1344f"
+          colorB="#e44019"
+          alpha={1}
+          mode="normal"
+          near={0.2}
+          far={1}
+          origin={[0.3, -0.3, 0]}
         />
-      </LayerMaterial>
+        <Fresnel
+          color="#fefefe"
+          alpha={0.54}
+          bias={0.1}
+          mode="overlay"
+          scale={1}
+        />
+      </DebugLayerMaterial>
       {data.map((props, i) => (
         <Sphere key={i} {...props} />
       ))}
