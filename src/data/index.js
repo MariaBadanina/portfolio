9dd8e672b168113dca44a2e74db8c66f153679b1
Maria <maria.badanina.s@gmail.com>
2023-10-31 22:29:50 -0400

    Change description in the work page

--- src/data/index.js
+++ src/data/index.js
@@ -4,12 +4,12 @@ const projects = [
     img: require('../static/img/project1.png'),
     tags: ['React.js', 'SASS', 'CSSModules', 'Storybook'],
     pageInfo:
-      "This poroject is a collection of some interactive UI components and modules I've created during my jorney as a Frontend developer working on [Griddo CMS](https://www.griddo.es/en/) Component System",
+      "This project is an example of some interactive UI components and modules I've created during my journey as a Frontend developer working on [Griddo CMS](https://www.griddo.es/en/) Component System",
     url: 'project-1',
     externalUrl: 'https://mariabadanina.github.io/component-collection',
     githubUrl: 'https://github.com/MariaBadanina/component-collection',
     description: `
-[Griddo CMS](https://www.griddo.es/en/) Component System was conceived with a vision of breaking free from rigid constraints, offering marketing teams a versatile set of building blocks that seamlessly integrate with one another. Each component has been meticulously crafted for high reusability, enabling a wide range of possibilities in website design. The ultimate goal was to build an extensive catalog of modules that not only simplify implementation but also respect and enhance the visual identity and brand guidelines of each of our clients.
+[Griddo CMS](https://www.griddo.es/en/) Component System was created to give marketing teams flexible building blocks that work well together. Each component is carefully designed for reuse, making it easy to design websites in different ways. Our main aim was to create a variety of modules that make implementation easier while matching our clients' brand guidelines.
 
 This task demanded a deep understanding of how these components would interact and adapt to various scenarios while preserving the brand's visual identity.`,
   },
